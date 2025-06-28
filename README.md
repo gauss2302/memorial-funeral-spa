778572ecbaac755f44294e860d00e87b49e43341
gauss2302 <shilov6865@gmail.com>
2025-06-28 12:38:26 +0500

    first prototype

---
+++ README.md
@@ -0,0 +1,21 @@
+# Memorial SPA
+
+A minimalistic SPA page for a funeral agency with core services. Yandex Map API is supported with the project.
+
+## Setup
+
+Make sure to install dependencies:
+
+```bash
+# npm
+npm install
+
+# pnpm
+pnpm install
+
+# yarn
+yarn install
+
+# bun
+bun install
+```
---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-layout">
+    <AppHeader />
+
+    <main class="main-content">
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Логика лейаута (если нужна)
+</script>
+
+<style scoped>
+.app-layout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.main-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+</style>
